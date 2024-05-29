575d66021142948330eab6d140c1800e1aa5d992
alilebbar <36114545+alilebbar@users.noreply.github.com>
2024-05-29 21:40:38 +0100

    application backend terminer avec socketIo

--- index.js
+++ index.js
@@ -15,80 +15,42 @@ const io = socketIo(server, {
 });
 
 app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
 mongoose
   .connect(
     "mongodb+srv://alilebbar94:mloolm123321@cluster0.3dfzznv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
-    console.log("connected");
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("not connected", error);
+    console.log("not connected to MongoDB", error);
   });
 
-//mongodb+srv://alilebbar94:<password>@cluster0.3dfzznv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-app.use(cors());
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-// recuperer les parametres avec l'option body
-
-app.get("/sayHello", (req, res) => {
-  const name = req.body.name;
-  res.send(`your name is ${name}`);
-});
-
-// recuperer les parametres avec l'option pathParameter
-app.get("/sumNumbers/:numb1/:numb2", (req, res) => {
-  let { numb1, numb2 } = req.params;
-  const sum = Number(numb1) + Number(numb2);
-  res.send(`${sum}`);
-});
-
-// recuperer les parametres avec l'option require Parameter
-app.get("/minNumbers", (req, res) => {
-  let { numb1, numb2 } = req.query;
-  const min = Number(numb1) - Number(numb2);
-  //res.send(`${min}`);
-  res.json({
-    result: min,
-    numbers: {
-      number1: numb1,
-      number2: numb2,
-    },
-  });
-});
-
-app.get("/numbers", (req, res) => {
-  let name = req.query.name;
-  let numbers = "";
-  for (let index = 0; index < 100; index++) {
-    numbers += index + "-";
+// Define routes
+app.get("/Articale", async (req, res) => {
+  try {
+    const articales = await Articale.find();
+    res.json(articales);
+  } catch (error) {
+    res.status(500).send(error);
   }
-  //res.send(numbers);
-  // res.send("<h1>hello world</h1>");
-  //res.sendFile(__dirname + "/views/index.html");
-  res.render("index.ejs", {
-    name: name,
-    numbers: numbers,
-  });
 });
 
 app.post("/Articale", async (req, res) => {
-  const articale = req.body;
-  const newArticale = new Articale();
-  newArticale.title = articale.title;
-  newArticale.body = articale.body;
-  newArticale.numberOfLike = articale.numberOfLike;
-  await newArticale.save();
-  io.emit("newArticale", newArticale);
-  //res.send("post");
-  res.status(201).json(articale);
+  try {
+    const newArticale = new Articale(req.body);
+    await newArticale.save();
+    io.emit("newArticale", newArticale); // Notify all clients about the new article
+    res.status(201).json(newArticale);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
+// Handle socket connections
 io.on("connection", (socket) => {
   console.log("New client connected");
 
@@ -97,31 +59,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/Articale", async (req, res) => {
-  const articales = await Articale.find();
-  res.json(articales);
-});
-
-app.get("/Articale/:articaleId", async (req, res) => {
-  const id = req.params.articaleId;
-  const articale = await Articale.findById(id);
-  res.json(articale);
-});
-
-app.delete("/Articale/:articaleId", async (req, res) => {
-  const id = req.params.articaleId;
-  const articale = await Articale.findByIdAndDelete(id);
-  res.json(articale);
-});
-
-app.post("/Articale/:articaleId", async (req, res) => {
-  const id = req.params.articaleId;
-  const articale = await Articale.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
-  res.json(articale);
-});
-
-app.get("/", (req, res) => {
-  res.send("hello world");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
