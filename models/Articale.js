2ad9c0401663a8af12e891fe505b907d5c89e1d8
alilebbar <36114545+alilebbar@users.noreply.github.com>
2024-05-29 23:34:07 +0100

    application backend terminer avec socketIo

--- models/Articale.js
+++ models/Articale.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articaleSchema = new Schema({
-  title: String,
   body: String,
-  numberOfLike: Number,
 });
 
 const Articale = mongoose.model("articale", articaleSchema);
